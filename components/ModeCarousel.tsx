46b59cce0bfce1166bdfd4218364f6942f3e2566
Yassine Zaanouni <yassinezaanouni@gmail.com>
2025-05-14 20:21:47 +0100

    rename images

--- components/ModeCarousel.tsx
+++ components/ModeCarousel.tsx
@@ -30,37 +30,37 @@ interface ImageInfo {
 
 const collectorImages: ImageInfo[] = [
   {
-    src: "/carousel/collector/title_technic_42126_buy_for_$75_compare_at_$160.webp",
+    src: "/carousel/collector/technic_42126.webp",
     title: "Technic 42126",
     buyPrice: "75",
     sellPrice: "160",
   },
   {
-    src: "/carousel/collector/title_macho_man_wwe_action_figure_buy_$28_compare_at_$75.webp",
+    src: "/carousel/collector/macho_man_wwe_action_figure.webp",
     title: "Macho Man WWE Action Figure",
     buyPrice: "28",
     sellPrice: "75",
   },
   {
-    src: "/carousel/collector/title_lego_star_wars_9488_buy_$78_compare_at_$170.webp",
+    src: "/carousel/collector/lego_star_wars_9488.webp",
     title: "Lego Star Wars 9488",
     buyPrice: "78",
     sellPrice: "170",
   },
   {
-    src: "/carousel/collector/title_funko_pop_gamer_bart_buy_$25_comapre_at_$50.webp",
+    src: "/carousel/collector/funko_pop_gamer_bart.webp",
     title: "Funko Pop Gamer Bart",
     buyPrice: "25",
     sellPrice: "50",
   },
   {
-    src: "/carousel/collector/title_carl_grimes_action_figure_buy_$75_compare_at_$120.webp",
+    src: "/carousel/collector/carl_grimes_action_figure.webp",
     title: "Carl Grimes Action Figure",
     buyPrice: "75",
     sellPrice: "120",
   },
   {
-    src: "/carousel/collector/title_barbie_cher_vintage_buy_$180_compare_at_$350.webp",
+    src: "/carousel/collector/barbie_cher_vintage.webp",
     title: "Barbie Cher Vintage",
     buyPrice: "180",
     sellPrice: "350",
@@ -69,37 +69,37 @@ const collectorImages: ImageInfo[] = [
 
 const flipperImages: ImageInfo[] = [
   {
-    src: "/carousel/flipper/title_radica_poker_vintage_game_buy_$30_sell_$65.webp",
+    src: "/carousel/flipper/radica_poker_vintage_game.webp",
     title: "Radica Poker Vintage Game",
     buyPrice: "30",
     sellPrice: "65",
   },
   {
-    src: "/carousel/flipper/title_panasonic_cassette_player_buy_$125_sell_$215.webp",
+    src: "/carousel/flipper/panasonic_cassette_player.webp",
     title: "Panasonic Cassette Player",
     buyPrice: "125",
     sellPrice: "215",
   },
   {
-    src: "/carousel/flipper/title_logiteh_650_harmony_buy_$130_sell_$290.webp",
+    src: "/carousel/flipper/logitech_650_harmony.webp",
     title: "Logitech 650 Harmony",
     buyPrice: "130",
     sellPrice: "290",
   },
   {
-    src: "/carousel/flipper/title_i_love_lucy_dvd_buy_$22_sell_$52.webp",
+    src: "/carousel/flipper/i_love_lucy_dvd.webp",
     title: "I Love Lucy DVD",
     buyPrice: "22",
     sellPrice: "52",
   },
   {
-    src: "/carousel/flipper/title_canon_ powershot_sd750_buy_$55_sell_$180.webp",
+    src: "/carousel/flipper/canon_powershot_sd750.webp",
     title: "Canon Powershot SD750",
     buyPrice: "55",
     sellPrice: "180",
   },
   {
-    src: "/carousel/flipper/title_brunswick_pro_bowling_ps4_buy_$23_sell_$65.webp",
+    src: "/carousel/flipper/brunswick_pro_bowling_ps4.webp",
     title: "Brunswick Pro Bowling PS4",
     buyPrice: "23",
     sellPrice: "65",
